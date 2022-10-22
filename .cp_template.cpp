81245275c5ed41a15df21b149d5851f155881aab
Dario Petrillo <dario.pk1@gmail.com>
2022-10-22 15:33:59 +0200

    Update template

--- .cp_template.cpp
+++ .cp_template.cpp
@@ -1,18 +1,18 @@
 #pragma GCC optimize("O3,inline")
+#pragma GCC target("avx2")
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
 #define all(c) begin(c),end(c)
 #define ssize(c) static_cast<int>(c.size())
-#define rep(var,start,end) for(int var = (start); var < (end); var++)
 
 using ll = long long;
 using pii = pair<int, int>;
-using vi = vector<int>;
-using vb = vector<bool>;
-using vll = vector<long long>;
-using vvi = vector<vector<int>>;
+using vi = vector<int>;  using vvi = vector<vector<int>>;
+using vb = vector<bool>; using vvb = vector<vector<bool>>;
+using vll = vector<ll>;  using vvll = vector<vector<ll>>;
+
+constexpr ll INF = 1e18;
 
 template<typename A, typename B> istream& operator>>(istream& s, pair<A,B>& p) { s >> p.first >> p.second; return s; }
 template<typename A, typename B> ostream& operator<<(ostream& s, const pair<A,B>& p) { s << "(" << p.first << "," << p.second << ")"; return s; }
@@ -25,8 +25,10 @@ ostream& operator<<(ostream& s, const Iterable& it)
     else { auto delim = ""; for(const auto& x : it) { s << delim << x; delim = " "; } }
     return s;
 }
+template<typename ReverseIterator> auto to_fwd_iterator(const ReverseIterator& it) { return std::next(it).base(); }
 
 void yesno(bool a) { cout << (a ? "YES\n" : "NO\n"); }
+void possible(bool a) { cout << (a ? "Possible\n" : "Impossible\n"); }
 auto micros() { return chrono::steady_clock::now().time_since_epoch() / chrono::microseconds{1}; }
 
 // ====================================================== //
@@ -39,7 +41,9 @@ void solve()
 
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     int T = 1;
     cin >> T;
